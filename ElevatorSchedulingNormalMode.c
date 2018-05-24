23b8857e5e11d86276d94bb7393adeb4f4ce2310
Vaibhav Bairagi <vaibhavbairagi105@gmail.com>
2018-05-24 09:08:36 +0530

    Added ElevatorSchedulingNormalMode.c

---
+++ ElevatorSchedulingNormalMode.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int compare (const void*x,const void *y)
+{
+	return (*(int*)x>*(int*)y)-(*(int*)y>*(int*)x);
+}
+int index(int b[],int n)
+{
+	int i=0;
+	for(i=0;b[i]!=n;i++);
+	return i;
+}
+void distance(int p[],int l){
+	int i,t=0;
+	for(i=1;i<l;i++){
+		if(p[i]>p[i-1])
+		t=t+p[i]-p[i-1];
+		if(p[i]<p[i-1])
+		t=t+p[i-1]-p[i];
+		
+}
+		printf("\nTotal distance = %d\n\n",t);
+
+}
+
+void scheduler(int a[],int l,int n)
+{
+	int b[l],p[l],i,j,k,c,key,m=n,t=0;
+	for(i=0;i<l;i++){
+		b[i]=a[i];
+	}
+	qsort(b,l,sizeof(int),compare);
+	key=index(b,m);
+	for(i=0;i<l;i++)
+	{
+		if(i<=l-key-1)
+			p[i]=b[i+key];
+		if(i>l-key-1)
+			p[i]=b[l-i-1];
+		printf("%d ",p[i]);
+	}
+			distance(p,l);
+
+	for(i=0;i<l;i++)
+	{
+		if(i<=key)
+			p[i]=b[key-i];
+		if(i>key)
+			p[i]=b[i];
+		printf("%d ",p[i]);
+	}
+			distance(p,l);
+}
+	
+
+int main()
+{
+	int n,a[100],i=0,j=0,x;
+	char t;
+	printf("Enter the initial floor number\n");
+	scanf("%d",&n);
+	printf("Enter the sequence of floor request\n");
+	do{
+		scanf("%d%c",&a[i],&t);
+		i++;
+	}
+	while(t!='\n');
+	int b[i+1];
+	b[0]=n;
+	for(j=0;j<i;j++)
+	b[j+1]=a[j];
+	scheduler(b,i+1,n);
+	
+}
