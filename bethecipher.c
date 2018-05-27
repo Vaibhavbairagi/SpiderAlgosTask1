9a5278859c2d9294abb4298640709b6a4fa16fe6
Vaibhav Bairagi <vaibhavbairagi105@gmail.com>
2018-05-27 10:26:20 +0530

    modified

--- bethecipher.c
+++ bethecipher.c
@@ -1,114 +1,47 @@
 #include<stdio.h>
 #include<string.h>
-void encrypt(char a[][100],int n, int k)
-{
-	char b[n/2][100];
-	int i,l,m,j,s,c,t,x[n];
-	m=k;
-	l=n/2;
-	if(m>26)
-	{
-		for(i=0;m>26;i++)
-		m=m-26;
+char cipher(char a, int k){
+    int b= (int) a, c;
+	if(b>=65&&b<=90){
+            c=b-65;
+            c=(c+k)%26;
+            c+=65;
+            return (char)c;
 	}
-	for(i=0;i<l;i++){
-		strcpy(b[i],a[i]);
-		for(j=0;a[i][j]!='\0';j++){
-			c=(int)b[i][j];
-			if(c>=65&&c<=90)
-			{
-				if(m<=90-c)
-				{
-					t=((int)b[i][j])+m;
-					b[i][j]=(char)t;
-				}
-				if(m>90-c)
-				{
-					t=((int)b[i][j])+m-26;
-					b[i][j]=(char)t;
-				}
-			}
-			if(c>=97&&c<=122)
-			{
-				if(m<=122-c)
-				{
-					t=((int)b[i][j])+m;
-					b[i][j]=(char)t;
-				}
-				if(m>122-c)
-				{
-					t=((int)b[i][j])+m-26;
-					b[i][j]=(char)t;
-				}
-			}
-
-		}
-		printf("Ciphertext# %d : %s\n",i+1,b[i]);
-
+    if(b>=97&&b<=122){
+            c=b-97;
+            c=(c+k)%26;
+            c+=97;
+            return (char)c;
+    }
+}
+void encrypt(char a[], int n, int k){
+	int i;
+    printf("ciphertext#%d:",n);
+    for(i=0;i<strlen(a);i++){
+        printf("%c",cipher(a[i],k));
 	}
 }
-void decrypt(char a[][100],int n, int k)
-{
-	char b[n/2][100];
-	int i,l,m,j,s,c,t;
-	m=k;
-	l=n/2;
-	if(m>26)
-	{
-		for(i=0;m>26;i++)
-		m=m-26;
-	}
-	for(i=0;i<l;i++){
-		strcpy(b[i],a[i+(n/2)]);
-		for(j=0;a[i][j]!='\0';j++){
-			c=(int)b[i][j];
-			if(c>=65&&c<=90)
-			{
-				if(m<=c-65)
-				{
-					t=((int)b[i][j])-m;
-					b[i][j]=(char)t;
-				}
-				if(m>c-65)
-				{
-					t=((int)b[i][j])-m+26;
-					b[i][j]=(char)t;
-				}
-			}
-			if(c>=97&&c<=122)
-			{
-				if(m<=c-97)
-				{
-					t=((int)b[i][j])-m;
-					b[i][j]=(char)t;
-				}
-				if(m>c-97)
-				{
-					t=((int)b[i][j])-m+26;
-					b[i][j]=(char)t;
-				}
-			}
-
-		}
-		printf("Plaintext# %d : %s\n",l-i+1,b[i]);
-
+void decrypt(char a[], int n, int k){
+	int i;
+    printf("plaintext#%d:",n);
+    for(i=0;i<strlen(a);i++){
+		printf("%c",cipher(a[i],26-k%26));
 	}
 }
 int main()
 {
-	int n,k,i,ln[n];
+	int n,k,i;
 	scanf("%d %d",&n,&k);
-	if(n%2==0)
-	{
-	    char input[n][100],str[100];
-		for(i=0;i<n;i++)
-		{
-			scanf(" %[^\n]s",input[i]);
-		}
-		encrypt(input,n,k);
-		decrypt(input,n,k);
+	getchar();
+	char input[50];
+	for(i=1;i<=n;i++){
+        fgets(input,sizeof(input),stdin);
+        encrypt(input,i,k);
+    }
+    for(i=1;i<=n;i++){
+        fgets(input,sizeof(input),stdin);
+        decrypt(input,i,k);
 	}
-	else
-	printf("Invalid input!! No. of test elements should be even");
 }
 
