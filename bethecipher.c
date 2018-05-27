491f5b0876e80cae51e1b43e42b8fdcce5df0d37
Vaibhav Bairagi <vaibhavbairagi105@gmail.com>
2018-05-27 17:24:01 +0530

    modified

--- bethecipher.c
+++ bethecipher.c
@@ -3,28 +3,24 @@
 char cipher(char a, int k){
     int b= (int) a, c;
 	if(b>=65&&b<=90){
-            c=b-65;
-            c=(c+k)%26;
-            c+=65;
+            c=((b-65+k)%26)+65;
             return (char)c;
 	}
     if(b>=97&&b<=122){
-            c=b-97;
-            c=(c+k)%26;
-            c+=97;
+            c=((b-97+k)%26)+97;
             return (char)c;
     }
 }
-void encrypt(char a[], int n, int k){
+void encrypt(char a[], int l, int k){
 	int i;
-    printf("ciphertext#%d:",n);
+    printf("ciphertext#%d:",l);
     for(i=0;i<strlen(a);i++){
         printf("%c",cipher(a[i],k));
 	}
 }
-void decrypt(char a[], int n, int k){
+void decrypt(char a[], int l, int k){
 	int i;
-    printf("plaintext#%d:",n);
+    printf("plaintext#%d:",l);
     for(i=0;i<strlen(a);i++){
 		printf("%c",cipher(a[i],26-k%26));
 	}
@@ -44,4 +40,3 @@ int main()
         decrypt(input,i,k);
 	}
 }
-
